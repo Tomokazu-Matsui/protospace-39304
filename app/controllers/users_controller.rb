5505be657ed0c3523f7f9c7ebf02ce666871056a
Tomokazu-Matsui <ruyuejiecheng@gmail.com>
2023-05-26 17:32:20 +0900

    修正５回目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @mame = current_user.name
-    @prototypes = current_user.prototypes
-    @profile = current_user.profile
-    @occupation = current_user.occupation
-    @position = current_user.position
-    
+    @user = User.find(params[id])
   end
   
 end
