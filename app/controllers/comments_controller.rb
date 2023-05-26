8dede6e47f7ee1e933786abf39f5612c4aed1d09
Tomokazu-Matsui <ruyuejiecheng@gmail.com>
2023-05-26 18:18:35 +0900

    修正７回目

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,12 +4,13 @@ class CommentsController < ApplicationController
     if @comment.save
     redirect_to "/prototypes/#{@comment.prototype.id}"
   else
-    @prototype= @comment.prototype
+    @prototype = @comment.prototype
     @comments = @prototype.comments
     render "prototypes#show" 
   end
+end
   private 
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
-
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
     devise_for :users
-    get 'prototypes/index'
     root to: "prototypes#index"
-    resources :prototypes do
+    resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
       resources :comments, only: :create
     end
-    resources :users, only: [:show, :edit, :update]
+    resources :users, only: :show
   end
